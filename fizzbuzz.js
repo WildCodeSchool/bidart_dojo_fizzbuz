a181617ccbce8049d1aaf180bb22390353522b15
Gagnant Nicolas <gagnantnicolas64@gmail.com>
2019-03-19 15:41:32 +0100

    creation florian nicolas fizzbzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,25 +2,57 @@
 
 
 
+// const fizzbuzz = (numb) => {
+//     let result = "";
+
+//     if (numb % 3 === 0 && numb % 5 === 0) {
+//         result = "fizzbuzz";
+//     }
+
+//     else if (numb % 5 === 0) {
+//         result = "buzz";
+//     }
+
+//     else if (numb % 3 === 0) {
+//         result = "fizz";
+//     }
+
+//     else {
+//         result = numb;
+//     }
+
+//     return result;
+// }
+
+// const fizzbuzz = (numb) => {
+//     let result = "";
+
+//     if (numb % 3 === 0) {
+//         result += "fizz";
+//     }
+
+//     if (numb % 5 === 0) {
+//         result += "buzz";
+//     }
+
+//     if (result == "") {
+//         result = numb;
+//     }
+
+//     return result;
+// }
+
+
+
 const fizzbuzz = (numb) => {
     let result = "";
 
-    if (numb % 3 === 0 && numb % 5 === 0) {
-        result = "fizzbuzz";
-    }
-
-    else if (numb % 5 === 0) {
-        result = "buzz";
-    }
-
-    else if (numb % 3 === 0) {
-        result = "fizz";
-    }
-
-    else {
-        result = numb;
-    }
-
+    result = (numb % 3 === 0 && numb % 5 === 0) ?
+        "fizzbuzz"
+        : (numb % 5 === 0) ?
+            "buzz"
+            : (numb % 3 === 0) ?
+                "fizz" : numb;
     return result;
 }
 
