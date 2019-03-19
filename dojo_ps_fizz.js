3731fbf220434808b0e07078476ab4bf2c0fc240
Sarah Elyafi <sarah.elyafi13@gmail.com>
2019-03-19 17:23:34 +0100

    adding dojo update exercise process.argv[]

--- dojo_ps_fizz.js
+++ dojo_ps_fizz.js
@@ -1,22 +1,23 @@
 // If else method
 
-// let fizzbuzz = number => {
-//     if (number % 3 == 0 && number % 5 == 0) {
-//         return ("fizzbuzz");
-//     } else if (number % 3 == 0) {
-//         return ("fizz");
-//     } else if (number % 5 == 0) {
-//         return ("buzz");
-//     } else {
-//         return (number);
-//     }
-// }
+let number = process.argv[2] // entrer le paramètre dans la console process.argv[]
+
+let fizzbuzz = number => {
+  if (number % 3 == 0 && number % 5 == 0) {
+    return 'fizzbuzz'
+  } else if (number % 3 == 0) {
+    return 'fizz'
+  } else if (number % 5 == 0) {
+    return 'buzz'
+  } else {
+    return number
+  }
+}
+console.log(fizzbuzz (number));
 
 // Switch case method
 
-console.log('Give me a number')
-
-let fizzbuzz = number => {
+/*let fizzbuzz = number => {
   switch (true) {
     case number % 3 == 0 && number % 5 == 0:
       return 'fizzbuzz'
@@ -31,14 +32,17 @@ let fizzbuzz = number => {
       return number
   }
 }
+/*
+
 
 
 // Test de 1 à 100:
 
-function test () {
+/* function test () {
   for (let i = 1; i < 100; i++) {
     console.log(fizzbuzz(i))
   }
 }
 
 test()
+*/
