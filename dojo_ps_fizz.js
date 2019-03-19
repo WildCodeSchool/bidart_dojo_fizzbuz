a1f1ad9fbebbcf55a55b3a3661df2bd35e950f0e
Sarah Elyafi <sarah.elyafi13@gmail.com>
2019-03-19 16:52:04 +0100

    adding dojo exercise fizzbuzz

---
+++ dojo_ps_fizz.js
@@ -0,0 +1,44 @@
+// If else method
+
+// let fizzbuzz = number => {
+//     if (number % 3 == 0 && number % 5 == 0) {
+//         return ("fizzbuzz");
+//     } else if (number % 3 == 0) {
+//         return ("fizz");
+//     } else if (number % 5 == 0) {
+//         return ("buzz");
+//     } else {
+//         return (number);
+//     }
+// }
+
+// Switch case method
+
+console.log('Give me a number')
+
+let fizzbuzz = number => {
+  switch (true) {
+    case number % 3 == 0 && number % 5 == 0:
+      return 'fizzbuzz'
+      break
+    case number % 5 == 0:
+      return 'buzz'
+      break
+    case number % 3 == 0:
+      return 'fizz'
+      break
+    default:
+      return number
+  }
+}
+
+
+// Test de 1 à 100:
+
+function test () {
+  for (let i = 1; i < 100; i++) {
+    console.log(fizzbuzz(i))
+  }
+}
+
+test()
