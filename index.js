6eea3b53f2fe12cedcb08fccd1a37c8bcc111b5f
Enzo Duhalde <enzo-duhalde@hotmail.fr>
2019-03-19 15:11:46 +0100

    upgraded files

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-function fizzBuzz(num) {
-  if (num % 3 == 0 && num % 5 == 0) {
-    return "fizzbuzz";
-  } else if (num % 5 == 0) {
-    return "buzz";
-  } else if (num % 3 == 0) {
-    return "fizz";
-  } else {
-    return num;
-  }
-}
+process.stdin.resume()
+process.stdin.setEncoding('utf8')
 
-console.log(fizzBuzz(100000000));
+console.log('Enter a number: ')
+process.stdin.on('data', (num) =>  {
+  if (num % 3 == 0 && num % 5 == 0) {
+    console.log( "fizzbuzz");
+  } else if (num % 5 == 0) {
+    console.log( "buzz");
+  } else if (num % 3 == 0) {
+    console.log( "fizz");
+  } else {
+    console.log(num);
+    process.exit()
+  }
+})
+
+
