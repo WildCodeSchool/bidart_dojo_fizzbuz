a772c2f1cb4ffbd8d34371f91c24d198481aae4a
Vanessa Salvador <salvadoribanez.vanessa@gmail.com>
2019-03-19 17:58:10 +0100

    Finish exercise

--- jean_vanessa.js
+++ jean_vanessa.js
@@ -4,25 +4,16 @@
 3 - "fizzbuzz" dans le cas où le nombre passé est à la fois un multiple de 3 et de 5
 4 - Le nombre dans le cas où ce dernier n’est ni un multiple de 3 ni un multiple de 5*/
 
-//Programme with function:
+//Programme with ternary function as light as possible:
 
-/*function fizzBuzz (num) {
-    if (num % 3 == 0 && num % 5 == 0) {
-        return 'fizzbuzz';
-    } else if (num % 3 == 0) {
-        return 'fizz';
-    } else if (num % 5 == 0) {
-        return 'buzz';
-    } else {
-        return num;
-    }
-}
-console.log(fizzBuzz(15));*/
+let fizzBuzz=(num)=>{return num%3==0&&num%5==0?'fizzbuzz':num%3==0?'fizz':num%5==0?'buzz':num}
+
+console.log(fizzBuzz(5));
 
 
-//Programme with for loop:
+//Programme with for loop and if / else conditions:
 
-for (num = 1; num<= 100; num ++) {
+/*for (num = 1; num<= 100; num ++) {
     if (num % 3 == 0 && num % 5 == 0) {
         console.log('fizzbuzz');
     } else if (num % 3 == 0) {
@@ -32,4 +23,32 @@ for (num = 1; num<= 100; num ++) {
     } else {
         console.log(num);
     }
-}
+}*/
+
+//Programme with for loop and switch:
+
+/*const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+readline.question(`Please, give a number between 1 and 100:`, (num) => {
+    switch (true) {
+        // If multiple 3 and 5
+        case num % 3 == 0 && num % 5 == 0: 
+        console.log('fizzbuzz');
+        break;
+        // If multiple 3
+        case num % 3 == 0 :
+        console.log('fizz');
+        break;
+        // If multiple 5
+        case num % 5 == 0:
+        console.log('buzz');
+        break;
+        // // If none
+        default :
+        console.log(num)
+    }
+    readline.close();
+})*/
