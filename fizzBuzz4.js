6d27b2de4e9f4164ba2179a10cd21d9cd7f769fa
Zesysy <47717972+Zesysy@users.noreply.github.com>
2019-03-19 18:11:03 +0100

    Update fizzBuzz4.js

--- fizzBuzz4.js
+++ fizzBuzz4.js
@@ -1,1 +1,1 @@
-function f(z){return z%3==0&&z%5==0?"fizzbuzz":z%5==0?"buzz":z%3==0?"fizz":z}for(let z=0;z<100;z++)f(z),console.log(f(z))
+z=>{return z%3==0&&z%5==0?"fizzbuzz":z%5==0?"buzz":z%3==0?"fizz":z};for(let z=0;z<100;z++)f(z),console.log(f(z))
