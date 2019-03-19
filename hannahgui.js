c43db040c34b4da7b76243f7c6ab85fcb80230be
Hannah Moncot <hannah.moncot@gmail.com>
2019-03-19 15:04:06 +0100

    guilhem et hannah js

---
+++ hannahgui.js
@@ -0,0 +1,12 @@
+function ezpz(number){
+    if (number%3 !=0){
+    console.log('fizz');
+    } else if (number%5 !=0){
+    console.log('buzz');
+    } else if ((i % 3 === 0 )||(i % 5 === 0)||(i % 3 === 0 && i % 5 === 0)){
+    sum = sum + i;
+    console.log('fizzbuzz');
+    } else {
+    console.log(number)
+    }
+    }
