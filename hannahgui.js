1209e6b155a8b3df8707fff00c12944098e44c06
Hannah Moncot <hannah.moncot@gmail.com>
2019-03-19 16:20:55 +0100

    another version

--- hannahgui.js
+++ hannahgui.js
@@ -11,4 +11,41 @@ function ezpz(number){
     }
     }
     console.log(number);
+
+
+    
+    const number = number;
+    switch (number){
+        case (number%3 !=0):
+            console.log('Fizz');
+            break;
+        case (number%5 !=0):
+            console.log('Buzz');
+            break;
+        case ((i % 3 === 0)||(i % 5 === 0)||(i % 3 === 0 && i % 5 === 0)):
+            console.log('fizzbuzz');
+            break;
+        case(number):
+            console.log(number);
+            break;            
+    }
+
+    function ezpz(number){
+        for (number=0; number<100; number++){
+            if (number%3 !=0){
+            return('fizz');
+            } else if (number%5 !=0){
+            return('buzz');
+            } else if ((i % 3 === 0 )||(i % 5 === 0)||(i % 3 === 0 && i % 5 === 0)){
+            sum = sum + i;
+            return('fizzbuzz');
+            } else {
+            return(number)
+            }
+            }
+    }
+        console.log(number);
+    
+
+
     
