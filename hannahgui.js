3a9efa8bc99385a9e513b6d464d9a0fa79818094
Hannah Moncot <hannah.moncot@gmail.com>
2019-03-19 17:11:01 +0100

    stdin /stdout

--- hannahgui.js
+++ hannahgui.js
@@ -46,6 +46,28 @@ function ezpz(number){
     }
         console.log(number);
     
+    process.stdin.resume()
+    process.stdin.setEncoding('utf8')
+        
+        
+    console.log('Enter a number')
+    process.stdin.on('data', (number) => {
+        switch (true) {
+            case number % 3 == 0 && number % 5 == 0 :
+                console.log('fizzbuzz');
+                break;
+            case number % 5 == 0 :
+                console.log('buzz');
+                break;
+            case number % 3 == 0 :
+                console.log('fizz');
+                break;
+                default :
+                    console.log(number);
+        }
+        process.exit()
+    })
+    
 
 
     
